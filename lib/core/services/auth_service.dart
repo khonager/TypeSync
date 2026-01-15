5bda9407d10aac16e097336956141f81575af119
name <name@mail.com>
2026-01-15 20:50:54 +0100

    fix url

--- lib/core/services/auth_service.dart
+++ lib/core/services/auth_service.dart
@@ -254,7 +254,7 @@ class AuthService extends ChangeNotifier {
       final acs = firebase.ActionCodeSettings(
         // URL you want to redirect back to. The domain (www.example.com) for this
         // URL must be whitelisted in the Firebase Console.
-        url: 'https://typesync.web.app/login?email=$email', // TODO: Configure dynamic link
+        url: 'https://typesynced.web.app/login?email=$email', // TODO: Configure dynamic link
         handleCodeInApp: true,
         iOSBundleId: 'com.khonager.typesync',
         androidPackageName: 'com.khonager.typesync',
